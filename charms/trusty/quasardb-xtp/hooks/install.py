abed0c3582db6d4439d6b80a06863e682342e90f
Edouard Alligand <edouard.alligand@bureau14.fr>
2015-10-05 11:34:42 +0200

    Update for quasardb 2.0.0 beta 5
     * Also added a download test

--- charms/trusty/quasardb-xtp/hooks/install.py
+++ charms/trusty/quasardb-xtp/hooks/install.py
@@ -1,21 +1,21 @@
 # change this if needed
-VERSION = "2.0/beta4"
+VERSION = "2.0/beta5"
 
 # you may want to change this
 
 SERVER_DEB          = "server/qdb-server_2.0.0-1.deb"
-SERVER_DEB_SHA1   = "3c2b700629a26b20b8d29eb220fc25af6cb403e8"
+SERVER_DEB_SHA1   = "2916489fefcd8aa0b59abdbd9b8d12f01e4463cd"
 
 UTILS_DEB           = "utils/qdb-utils_2.0.0-1.deb"
-UTILS_DEB_SHA1    = "b0c15732a03aa6c4221f5195c0cebd0883e8e30c"
+UTILS_DEB_SHA1    = "15000a746527ace2f9d4de95bc2b254105d0c927"
 
 CAPI_DEB            = "api/c/qdb-api_2.0.0-1.deb"
-CAPI_DEB_SHA1     = "2a9b6bcd4660a04f20eccaf1ee8119c9fa0bf862"
+CAPI_DEB_SHA1     = "e66b6e27ae19126c7ca25ea21a04fd809cb5d0f3"
 
 # don't change this
-__base_dl_url = "https://download.quasardb.net/quasardb/"
+BASE_DL_URL = "https://download.quasardb.net/quasardb/"
 
-__debs = { 
+DEBS = { 
     SERVER_DEB: SERVER_DEB_SHA1,
     UTILS_DEB: UTILS_DEB_SHA1,
     CAPI_DEB: CAPI_DEB_SHA1
@@ -26,7 +26,7 @@ from charmhelpers.fetch.archiveurl import ArchiveUrlFetchHandler
 import os.path
 
 def full_url(deb):
-    return __base_dl_url + VERSION + "/" + deb
+    return BASE_DL_URL + VERSION + "/" + deb
 
 def install_deb(deb):
     import subprocess
@@ -37,5 +37,5 @@ def install():
     h = ArchiveUrlFetchHandler()
 
     # download, validate, install
-    for deb, sha in __debs.iteritems():
+    for deb, sha in DEBS.iteritems():
         install_deb(h.download_and_validate(full_url(deb), sha, validate="sha1"))
